a6916396be863a6c2be46910e98414dd2e5e147b
Stujo <email": null>
2015-12-30 14:36:38 -0800

    Initial Commit

---
+++ cpp-hello-world/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpp-hello-world
+//
+//  Created by Stuart on 12/30/15.
+//  Copyright © 2015 stujo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
